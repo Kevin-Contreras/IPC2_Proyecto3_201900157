2ad84eca4bc8cb8c7678e1779e4672d664687e49
Kevin <kevinacontrerasl62@gmail.com>
2021-05-09 19:40:08 -0600

    vistas terminadas

--- Vista/Vista/urls.py
+++ Vista/Vista/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Vista.view import vista,archivo1,archivo2
+from Vista.view import vista,archivo1,archivo2,operaciones,filtar,filtar2
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',vista),
     path('cargarArchivo/',archivo1),
-    path('cargarArchivo/add',archivo2),
+    path('/',archivo2),
+    path("operaciones/",operaciones),
+    path("operaciones/filtarDatos",filtar),
+    path("operaciones/filtarError",filtar2),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
--- Vista/Vista/view.py
+++ Vista/Vista/view.py
@@ -1,24 +1,44 @@
 from django.http import HttpResponse
 from django.template import Template,Context 
+from django.shortcuts import redirect
+
 def vista(request):
- Principal= open("C:/Users/kevin/Desktop/PROYECTO 3/Vista/Vista/Plantillas/index.html")
+ Principal= open("../Vista/Vista/Plantillas/index.html")
  temp = Template(Principal.read())
  Principal.close()
  ctx = Context()
  return HttpResponse(temp.render(ctx))
 def archivo1(request):
-  Principal= open("C:/Users/kevin/Desktop/PROYECTO 3/Vista/Vista/Plantillas/archivo.html")
+  Principal= open("../Vista/Vista/Plantillas/archivo.html")
   temp = Template(Principal.read())
   Principal.close()
   ctx = Context()
   return HttpResponse(temp.render(ctx))  
 
 def archivo2(request):
-  Principal= open("C:/Users/kevin/Desktop/PROYECTO 3/Vista/Vista/Plantillas/archivo.html")
+
+  xml=""
+  archivo= open("../Vista/Vista/archivos/"+request.POST["CARGAR_ARCHIVO"],encoding="utf8")
+  for linea in archivo.readlines():
+    print(xml)
+    xml = xml+linea +" "
+  print(xml)
+  return redirect("/")
+def operaciones(request):
+  Principal= open("../Vista/Vista/Plantillas/operaciones.html")
   temp = Template(Principal.read())
   Principal.close()
   ctx = Context()
-  
- 
-  
+  return HttpResponse(temp.render(ctx))  
+def filtar(request):
+  Principal= open("../Vista/Vista/Plantillas/filtrar.html")
+  temp = Template(Principal.read())
+  Principal.close()
+  ctx = Context()
+  return HttpResponse(temp.render(ctx))  
+def filtar2(request):
+  Principal= open("../Vista/Vista/Plantillas/filtro2.html")
+  temp = Template(Principal.read())
+  Principal.close()
+  ctx = Context()
   return HttpResponse(temp.render(ctx))  
